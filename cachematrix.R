95982776a5ef9216378be16c55584dc0b4946d65
zensualito <zensualito@gmail.com>
2016-08-04 22:57:50 -0500

    Added assigment description.

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,19 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-
+#+-------------------------------------------------------+
+#| Matrix inversion is usually a costly computation and  |
+#| there may be some benefit to caching the inverse      |
+#| of a matrix rather than compute it repeatedly         |
+#| (there are also alternatives to matrix inversion      |
+#| that we will not discuss here).                       |
+#|                                                       |
+#| Computing the inverse of a square matrix can be done  |
+#| with the solve function in R.                         |
+#|                                                       |
+#| For example, if X is a square invertible matrix,      |
+#| then solve(X) returns its inverse.                    |
+#|                                                       |
+#| For this assignment,                                  |
+#| assume that the matrix supplied is always invertible. |
+#+-------------------------------------------------------+
 
 
 ## Write a short comment describing this function
